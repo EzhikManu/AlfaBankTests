7fae61e1c04b4a2ae366b5427ea0798412cd2462
EzhikManu <iljasstan@mail.ru>
2021-10-18 16:55:27 +0300

    added testCreditCalc()

--- src/test/java/com/iljasstan/AlfaBankTests.java
+++ src/test/java/com/iljasstan/AlfaBankTests.java
@@ -1,9 +1,13 @@
 package com.iljasstan;
 
 import com.codeborne.selenide.Configuration;
+import com.iljasstan.enums.TariffConditions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
@@ -21,7 +25,7 @@ public class AlfaBankTests {
     void testCreditCardApplicationForm0() {
         step("open main page and click 'Интернет-банк'", () -> {
                     open("https://alfabank.ru");
-                    $(".I1Cda9").click();
+                    $(".H1Cda9").click();
                 });
         step("click 'стать клиентом'", () -> {
                     $(".browser-header__link").click();
@@ -55,9 +59,45 @@ public class AlfaBankTests {
             open("https://alfabank.ru");
         });
         step("set to search field value 'кредит наличными'", () -> {
+                    $(".a3qH-M").click();
                     $("[type=search]").setValue("кредит наличными").pressEnter();
                 });
-        $("#ya-site-results").shouldHave(text("кредит наличными"));
-
+        step("check, that the result has value 'кредит наличными'", () -> {
+            $("#ya-site-results").shouldHave(text("кредит наличными"));
+        });
     }
-}
+
+    @ParameterizedTest
+    @EnumSource(TariffConditions.class)
+    @DisplayName("Test, that just 1% tariff condition {0} are on the page")
+    void tariffConditionsTest(TariffConditions condition) {
+        step("open main page and click to 'Малому бизнесу и ИП'", () -> {
+            open("https://alfabank.ru");
+            $(byText("Малому бизнесу и ИП")).click();
+        });
+        step("open just 1% tariff conditions", () -> {
+            $$(byPartialLinkText("Подробнее")).first().click();
+        });
+        step("check, that all conditions are on the page", () -> {
+            $(".aGTC57").shouldHave(text(condition.getDesc()));
+        });
+        }
+
+        @Test
+    void testCreditCalc() {
+            step("open main page", () -> {
+                open("https://alfabank.ru");
+            });
+            step("set values to credit calc", () -> {
+                $("#creditAmount").doubleClick().sendKeys(Keys.BACK_SPACE);
+                $("#creditAmount").doubleClick().sendKeys(Keys.BACK_SPACE);
+                $("#creditAmount").doubleClick().sendKeys(Keys.BACK_SPACE);
+
+                $("#creditAmount").setValue("2500000");
+                $("[data-test-id='creditTerm-4']").click();
+            });
+            step("check the monthly payment", () -> {
+                $$(".aJuYLG").first().shouldHave(text("63 900 ₽"));
+            });
+        }
+    }
---
+++ src/test/java/com/iljasstan/enums/TariffConditions.java
@@ -0,0 +1,32 @@
+package com.iljasstan.enums;
+
+public enum TariffConditions {
+    CONDITION0("Обслуживание счёта"),
+    CONDITION1("Переводы юрлица и ИП"),
+    CONDITION2("""
+            Переводы на счета физлиц
+
+            """),
+    CONDITION3("""
+            Переводы со счёта ИП на личный счёт в Альфа-Банке
+
+            """),
+    CONDITION4("""
+            Платежи по бумажным поручениям
+
+            """),
+    CONDITION5("""
+            Налоговые и бюджетные платежи
+
+            """);
+
+    String desc;
+
+    TariffConditions(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
