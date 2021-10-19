4f1943c5ba6b2eb34731f1125b8ec45c7b90ba26
EzhikManu <iljasstan@mail.ru>
2021-10-19 15:56:35 +0300

    try to edit video url

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -44,7 +44,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl(String sessionId) {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
+        String videoUrl = "http://161.35.194.216:8080/#/video/" + sessionId + ".mp4";
 
         try {
             return new URL(videoUrl);
